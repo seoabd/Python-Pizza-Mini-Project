9f964b92277f999a29a50282333000736ca7eb3a
Sead Abduli <abdulisead@icloud.com>
2025-12-07 19:25:34 +0100

    Add files via upload

---
+++ Python Pizza Project.py
@@ -0,0 +1,27 @@
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size of a pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ").lower()
+extra_cheese = input("Do you want extra cheese? Y or N: ").lower()
+
+bill = 0
+
+if size == "s":
+    bill += 15
+
+elif size == "m":
+    bill += 20
+
+elif size == "l":
+    bill += 25
+else:
+    print("Sorry, you typed a wrong letter")
+
+if pepperoni == "y":
+    if size == "s":
+        bill += 2
+    else:
+        bill += 3
+if extra_cheese == "y":
+        bill += 1
+
+print(f"Your total bill is {bill}$ and bonna Appetit")
